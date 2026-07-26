0416c391bd2248d0e58466a33764b5dd5959a60f
mrj0b <djeberboudiba@hotmail.com>
2026-07-26 06:42:56 +0100

    fix(rtl): set dir attribute on CoralShadowRootContainer for Shadow DOM RTL scoping

--- client/src/core/client/ui/encapsulation/CoralShadowRootContainer.tsx
+++ client/src/core/client/ui/encapsulation/CoralShadowRootContainer.tsx
@@ -1,5 +1,6 @@
 import React, { FunctionComponent } from "react";
 
+import { useUIContext } from "../components/v2/UIContext/UIContext";
 import { withForwardRef } from "../hocs";
 import DivWithShadowBreakpointClasses from "./DivWithShadowBreakpointClasses";
 
@@ -11,16 +12,21 @@ type Props = React.HTMLAttributes<HTMLDivElement> & {
  * Coral Entrypoint Container rendered into the Shadow DOM.
  * It's basically a div that sets `id="coral"`
  * and adds breakpoint classNames.
+ * It also sets the `dir` attribute for RTL support inside the Shadow DOM,
+ * since [dir="rtl"] selectors from postcss-rtlcss need an ancestor with
+ * the dir attribute within the same shadow tree.
  */
 const CoralShadowRootContainer: FunctionComponent<Props> = ({
   forwardRef,
   ...rest
 }) => {
+  const { dir } = useUIContext();
   return (
     <DivWithShadowBreakpointClasses
       ref={forwardRef}
       {...rest}
       id="coral"
+      dir={dir}
       // exclude Coral from snippets
       data-nosnippet
     />
